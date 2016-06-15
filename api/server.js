8d084154d0d588d33cb9d9d04bf3d13bc3415b8f
ghamize <hamidighezal@gmail.com>
2016-06-15 15:24:15 -0400

    server

---
+++ api/server.js
@@ -0,0 +1,18 @@
+//initialize express
+var express = require('express');
+
+//run express
+var app = express();
+
+//api config
+app.use(express.static(__dirname + './../'));
+
+//change port to 80
+var PORT = 80;
+
+//responses
+app.listen(80,function(){
+	console.log('Server started on http://localhost:8080');
+	console.log('Press CTRL + C to stop server');
+
+});
